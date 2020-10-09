d6dd020c9af5f392aa5c8459a70283af46c33c75
gabrielmacey <gabem0928@yahoo.com>
2020-10-09 11:23:56 -0400

    pyBank

--- PyBank/main.py
+++ PyBank/main.py
@@ -4,26 +4,22 @@ import csv
 #Join the CSV files
 pybank_csv = os.path.join('Resources', 'PyBank.csv')
 
+new_pybank = []
 #Read the CSV files and check to see the types 
 with open(pybank_csv) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
+    csvheader = next(csvreader)
 
     #List representation of CSV file
     pybank = list((csvreader))
 
     #Find the length of the list and subtract one for header
-    total_months = len(pybank)-1
+    total_months = len(pybank)
 
     #Print out number of months
-    print("Total Months:", total_months)
+    print('Total Months:', total_months)
 
     #Print out the sum of Profit/Losses
-    del pybank(0)
-    column = 1
-    print(sum(row[1] for row in pybank))
-    net_total = (pybank[]) #This gives me the first row
-    print(net_total)
-
-## total_net = sum()
-## print("The total net gain/loss is:", total_net)
-
+    Floats = float(pybank['Profit/Losses'])
+    #total = sum(float(row["Profit/Losses"]) for row in reader)
+    #print("Total: $" + format(str(total))+"0")
