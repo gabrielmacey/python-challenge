944de7320e425e7512366c70059e1bf875d10a3b
gabrielmacey <gabem0928@yahoo.com>
2020-10-10 14:07:18 -0400

    pybankwork

--- PyBank/main.py
+++ PyBank/main.py
@@ -5,6 +5,7 @@ import csv
 pybank_csv = os.path.join('Resources', 'PyBank.csv')
 
 new_pybank = []
+total_profits = []
 #Read the CSV files and check to see the types 
 with open(pybank_csv) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
@@ -18,8 +19,3 @@ with open(pybank_csv) as csvfile:
 
     #Print out number of months
     print('Total Months:', total_months)
-
-    #Print out the sum of Profit/Losses
-    Floats = float(pybank['Profit/Losses'])
-    #total = sum(float(row["Profit/Losses"]) for row in reader)
-    #print("Total: $" + format(str(total))+"0")
