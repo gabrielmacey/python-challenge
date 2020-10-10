74d9d57a525c79cd79adbd513fd5839a90fd4146
gabrielmacey <gabem0928@yahoo.com>
2020-10-10 14:46:12 -0400

    pybankwork

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,6 +1,7 @@
 import os
 import csv
 
+total_months = 0
 net_total = 0
 last_net = 0
 increase = 0
@@ -23,14 +24,19 @@ total_profits = []
 with open(pybank_csv) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     csvheader = next(csvreader)
+    firstrow = next(csvreader)
+    print(f'CSV Header: {firstrow}')
+    print(type(firstrow))
+    total_months+=1
     #List representation of CSV file
-    pybank = list((csvreader))
-    #Find the length of the list and subtract one for header
-    total_months = len(pybank)
-    #Print out number of months
-    print('Total Months:', total_months)
+    
 
-    for row in csvreader:
-        date.append(row[0])
-        profit.append(int(row[1]))
+    net_total += int(firstrow[1])
+    last_net = int(firstrow[1])
+    #Print out number of months
+    #print('Total Months:', total_months)
+
+    #for row in csvreader:
+        #date.append(row[0])
+        #profit.append(int(row[1]))
         
